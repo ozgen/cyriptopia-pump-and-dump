c4ae9f2740bbc219c5df40320a2db6b638871c10
Ozgen <ozgn45@gmail.com>
2018-03-01 00:56:49 +0300

    script is added

--- Runner.py
+++ Runner.py
@@ -1,12 +1,9 @@
-import  CyriptopiaPumpAndDump
+import CyriptopiaPumpAndDump
 import argparse
 
-
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser()
     parser.add_argument('--symbol', type=str, help='Market Symbol (Ex: XVGBTC - XVGETH)', required=True)
 
     coin_name = parser.parse_args()
     CyriptopiaPumpAndDump.PumpDumpCyriptopia().pumpDump(str(coin_name.symbol).upper())
-
